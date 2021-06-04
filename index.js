e20e5f0fb6ebec154b22c9fc4dd2ab63cdb64dc3
zeejay21 <>
2021-06-04 00:02:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Declare and initialize the variables for exercise 1 here:
+
+
+
+// BEFORE running the code, predict what will be printed to the console by the following statements:
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+
+if (engineIndicatorLight === "green") {
+   console.log("engines have started");
+} else if (engineIndicatorLight === "green blinking") {
+   console.log("engines are preparing to start");
+} else {
+   console.log("engines are off");
+}
